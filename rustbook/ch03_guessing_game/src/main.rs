55a5177648bd75a6acd6bf32b2930763a7644e1b
NoSignificantWorries <dmitry.karpachev@hotmail.com>
2026-01-10 00:22:27 +0700

    updated

---
+++ rustbook/ch03_guessing_game/src/main.rs
@@ -0,0 +1,35 @@
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    let secret = rand::random_range(1..=100);
+    println!("The secret number is: {secret}");
+
+    println!("Guessing the number!");
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read your guess");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
